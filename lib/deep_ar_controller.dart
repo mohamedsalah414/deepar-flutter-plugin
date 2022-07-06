c7874a61cfa9fa44e218ac7840a5e09a4a0fc0bf
Wilson Christian <59436377+wilson-christian@users.noreply.github.com>
2022-07-06 17:22:14 +0530

    Code refactored & removed unused logics

--- lib/deep_ar_controller.dart
+++ lib/deep_ar_controller.dart
@@ -1,4 +1,3 @@
-import 'package:camera/camera.dart';
 import 'package:deep_ar/deep_ar_platform_handler.dart';
 import 'package:flutter/material.dart';
 
@@ -9,42 +8,16 @@ class DeepArController {
 
   bool get isInitialized => textureId != null;
 
-  Future<void> initialize() async {
-    await _deepArPlatformHandler.initialize();
-    textureId = await _deepArPlatformHandler.startCamera();
-    print("textureID $textureId");
+  Future<void> initialize({required String licenseKey}) async {
+    bool? isInitialized = await _deepArPlatformHandler.initialize(licenseKey);
+    if (isInitialized != null && isInitialized) {
+      textureId = await _deepArPlatformHandler.startCamera();
+    }
   }
-
-  bool sendFrames = true;
 
   Widget buildPreview() {
-    // super.startImageStream((image) async {
-    //   if (sendFrames) {
-    //     _deepArPlatformHandler.receiveFrame(image);
-    //   }
-    // });
-
     return Texture(textureId: textureId!);
   }
-
-  @override
-  Future<void> startVideoRecording() async {
-    // TODO: implement startVideoRecording
-    // return super.startVideoRecording();
-  }
-  @override
-  Future<void> resumePreview() async {
-    // super.startImageStream((image) async {
-    //   if (sendFrames) {
-    //     _deepArPlatformHandler.receiveFrame(image);
-    //   }
-    // });
-  }
-
-  // Future<void> pausePreview() async {
-  //   stopImageStream();
-  //   // return super.pausePreview();
-  // }
 
   Future<String?> switchEffect(int effect) {
     return _deepArPlatformHandler.switchEffect(effect);
